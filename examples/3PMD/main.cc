793514255cbb0c9f7f8eb15071d6e0098c197e33
icosac <enricosaccon96@gmail.com>
2025-03-06 14:33:27 +0100

    Working on ML/DL

--- examples/3PMD/main.cc
+++ examples/3PMD/main.cc
@@ -18,6 +18,6 @@ int main(int argc, char** argv){
 	generateDataset3PDPCircle(argc, argv);
 	// generateDataset3PDPRect(argc, argv);
 
-	generateDataset3PDPCircleTest(argc, argv);
+	// generateDataset3PDPCircleTest(argc, argv);
 	return 0;
 }
